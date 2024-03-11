fdcede2c4a49366b301d4714020e78d90ebaad2f
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-03-11 12:07:19 +0100

    removed whitespace

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SECONDS = 0
-
 # Preparation and setup of required files 
 
 FILES=$(pwd)
@@ -20,55 +18,51 @@ PICARD=$WKDIR/required_files/picard.jar
 
 echo 'Are the data in bam or fastq format?'
 read -p 'Specify file format (bam or fastq): ' FORMAT
-
 read -p 'Do you want to do a quality control of the raw data (yes or no): ' QCRAW
-
 read -p 'Are the libraries prepared in a strand-specific way? (yes or no): ' STRANDED
-
 read -p 'Are the data paired-end? (yes or no): ' PAIRED
-
 read -p 'How many threads (cores) should be used for the analysis (use 1 if you are not sure): ' THREAD
 
 
 #### (1) QC of raw data ####
 
 if [ $QCRAW == 'yes' ]
+then 
+mkdir $WKDIR/QC_raw
+echo 'Quality control of raw data:'
+if [ $FORMAT == 'bam' ]
 then
-	mkdir $WKDIR/QC_raw
-	echo 'Quality control of raw data:'
-	if [ $FORMAT == 'bam' ]
-	then
-		for i in $WKDIR/*.bam
-		do
-			fastqc -o $WKDIR/QC_raw $i
-		done
-	else
-		for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
-		do
-			i=$WKDIR/$SNAME
-			fastqc -o $WKDIR/QC_raw $i
-		done
-	fi
-	multiqc -o $WKDIR/QC_raw $WKDIR/QC_raw
+for i in $WKDIR/*.bam
+do
+fastqc -o $WKDIR/QC_raw $i
+done
+else	
+for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(q\.gz$)')
+do
+i=$WKDIR/$SNAME
+fastqc -o $WKDIR/QC_raw $i
+done
+fi
+multiqc -o $WKDIR/QC_raw $WKDIR/QC_raw
 else
-	echo 'No QC of raw data done.'
+echo 'No QC of raw data done.'
 fi
 
 # Convert .bam to .fastq format
 
 if [ $FORMAT == 'bam' ]
 then
-	echo 'File format is bam.'
-	for i in $WKDIR/*.bam
-	do
-		bamToFastq -i $i -fq $i.fq
-	done
+echo 'File format is bam.'
+for i in $WKDIR/*.bam
+do
+bamToFastq -i $i -fq $i.fq
+done
 elif [ $FORMAT == 'fastq' ]
 then
-	echo 'File format is fastq.'
+echo 'File format is fastq.'
 else
-	echo 'Invalid file format! Options are "bam" or "fastq".'
-	exit
+echo 'Invalid file format! Options are "bam" or "fastq".'
+exit
 fi
 
 
@@ -77,28 +71,27 @@ fi
 
 for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(L*_1\.fq\.gz$)')
 do
-	i1=$WKDIR/$SNAME
-	i2=$(echo $i1| sed 's/_1.fq.gz/_2.fq.gz/')
-
-	cutadapt -j $THREAD -q 30 -O 1 -a $ADAPT5 -A $ADAPT3 -o $i1.fq.gz -p $i2.fq.gz $i1 $i2  
-	echo "Adapters trimmed."
-
+i1=$WKDIR/$SNAME
+i2=$(echo $i1| sed 's/_1.fq.gz/_2.fq.gz/')
+cutadapt -j $THREAD -q 30 -O 1 -a $ADAPT5 -A $ADAPT3 -o $i1.fq.gz -p $i2.fq.gz $i1 $i2  
+echo "Adapters trimmed."
+done
 
 #### (3) Mapping of all files with HISAT2 ####
-	hisat2 -x $GENOME -1 $i1.fq.gz -2 $i2.fq.gz -S $i1.trimmed.sam --threads $THREAD --phred33
+hisat2 -x $GENOME -1 $i1.fq.gz -2 $i2.fq.gz -S $i1.trimmed.sam --threads $THREAD --phred33
 
-	samtools sort -@ $THREAD $i1.trimmed.fq.bam -o $i1.trimmed.fq.bam.sort.bam   # sort .bam files using samtools
-	mv $i1.count.txt $WKDIR/required_files
+samtools sort -@ $THREAD $i1.trimmed.fq.bam -o $i1.trimmed.fq.bam.sort.bam   # sort .bam files using samtools
+mv $i1.count.txt $WKDIR/required_files
 	
-	bedtools intersect -a $i1.trimmed.fq.bam.sort.bam -b $rRNA -v > $i1.trimmed.fq.bam.sort.bam.rRNAfilt.bam  # removal of reads mapping to rRNA loci
+bedtools intersect -a $i1.trimmed.fq.bam.sort.bam -b $rRNA -v > $i1.trimmed.fq.bam.sort.bam.rRNAfilt.bam  # removal of reads mapping to rRNA loci
 
-	# Labelling of duplicated reads and removal of optical duplicates
-	java -jar $PICARD MarkDuplicates REMOVE_SEQUENCING_DUPLICATES=true I=$i1.trimmed.fq.bam.sort.bam.rRNAfilt.bam O=$i1.final.bam M=$WKDIR/QC/$SNAME.markdup.metrics.txt
+# Labelling of duplicated reads and removal of optical duplicates
+java -jar $PICARD MarkDuplicates REMOVE_SEQUENCING_DUPLICATES=true I=$i1.trimmed.fq.bam.sort.bam.rRNAfilt.bam O=$i1.final.bam M=$WKDIR/QC/$SNAME.markdup.metrics.txt
 	
-	#Quality control and statistics about mapped samples
-	samtools flagstat $i1.final.bam >> $WKDIR/QC/$SNAME.final.flagstat_analysis.txt   # flagstat analysis
+#Quality control and statistics about mapped samples
+samtools flagstat $i1.final.bam >> $WKDIR/QC/$SNAME.final.flagstat_analysis.txt   # flagstat analysis
 
-	fastqc -o $WKDIR/QC $i1.final.bam
+fastqc -o $WKDIR/QC $i1.final.bam
 
 done
 
@@ -112,29 +105,29 @@ mkdir $WKDIR/count
 mkdir $WKDIR/diff_expr_analysis
 
 for i in $WKDIR/*.markdup.bam
-  do
-  htseq-count -f bam -s no -t gene -i ID $i $FEATURES_H > $i.count.txt
-  mv $i.count.txt $WKDIR/count
-  echo "Human transcripts done"
+do
+htseq-count -f bam -s no -t gene -i ID $i $FEATURES_H > $i.count.txt
+mv $i.count.txt $WKDIR/count
+echo "Human transcripts done"
 done
 
 for i in $WKDIR/*.markdup.bam
-  do
-  htseq-count -f bam -s no -t gene -i ID $i $FEATURES_C > $i.count.txt
-  mv $i.count.txt $WKDIR/count
-  echo "Candida transcripts done"
+do
+htseq-count -f bam -s no -t gene -i ID $i $FEATURES_C > $i.count.txt
+mv $i.count.txt $WKDIR/count
+echo "Candida transcripts done"
 done
 
 for i in $WKDIR/*.markdup.bam
-  do
-  htseq-count -f bam -s no -t gene -i ID $i $FEATURES_S > $i.count.txt
-  mv $i.count.txt $WKDIR/count
-  echo "Saccharomyces transcripts done"
+do
+htseq-count -f bam -s no -t gene -i ID $i $FEATURES_S > $i.count.txt
+mv $i.count.txt $WKDIR/count
+echo "Saccharomyces transcripts done"
 done
 
 for i in $WKDIR/count/*.count.txt
 do
-	head -n -5 $i > $i.crop.txt  # clear count files for flags
+head -n -5 $i > $i.crop.txt  # clear count files for flags
 done
 
 cp $WKDIR/count/*.crop.txt $WKDIR/diff_expr_analysis
@@ -147,10 +140,7 @@ mkdir $WKDIR/IGV_files
 
 for i in $WKDIR/*.markdup.bam
 do
-	samtools index $i
-	SNAME=$(echo $i | sed 's:/.*/::g')
-	bamCoverage -b $i -o $WKDIR/IGV_files/$SNAME.bw --normalizeUsing CPM -p $THREAD
+samtools index $i
+SNAME=$(echo $i | sed 's:/.*/::g')
+bamCoverage -b $i -o $WKDIR/IGV_files/$SNAME.bw --normalizeUsing CPM -p $THREAD
 done
-
-duration=$SECONDS
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
