68462429af3ba51b90b9b669c604ebe64f25efd5
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-04-16 21:37:41 +0200

    Create coverage.sh

---
+++ coverage.sh
@@ -0,0 +1,15 @@
+# Get the coverage percentages
+
+for SNAME in $(ls | egrep '\.bam$') 
+do 
+i=$SNAME 
+bedtools genomecov -ibam $i > $i.coverage.txt 
+done
+
+# Manually compile 0-4X coverage over the whole genome (replace chromosomename by the name of each chromosome)
+
+for SNAME in $(ls | egrep '\.coverage.txt$') 
+do 
+i=$SNAME 
+grep 'chromosomename' $i | head -5 >> $i.crop.txt 
+done 
