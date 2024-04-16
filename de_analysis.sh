09bb8b1403a4b242ea82b6621f862c2bc9d5dc49
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-04-16 21:30:26 +0200

    Update de_analysis.sh

--- de_analysis.sh
+++ de_analysis.sh
@@ -5,3 +5,10 @@ WKDIR=$(echo $FILES | sed 's:/required_files::g')
 cp $WKDIR/count/*.crop.txt $WKDIR/diff_expr_analysis
 cp $FILES/edgeR_analysis.R $WKDIR/diff_expr_analysis
 cp $FILES/Targets.txt $WKDIR/diff_expr_analysis
+
+for i1 in $WKDIR/*.final.bam
+do
+samtools index $i1
+SNAME=$(echo $i1 | sed 's:/.*/::g')
+bamCoverage -b $i1 -o $WKDIR/IGV_files/$SNAME.bw --normalizeUsing CPM -p 15 -e
+done
