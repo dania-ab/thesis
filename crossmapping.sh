b1ff233c3d4ac45273b2c3b9338280812d2d3d96
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-04-18 09:51:05 +0200

    Update crossmapping.sh

--- crossmapping.sh
+++ crossmapping.sh
@@ -1,1 +1,3 @@
 # https://github.com/Gabaldonlab/crossmapper
+
+crossmapper RNA -t 4 -rlay PE -rlen 150 -o ~/Desktop -rc TRUE -gn GenomeName1 GenomeName2 GenomeName3 -g fasta Fasta1 Fasta2 Fasta3  -a gtf GTF1 GTF2 GTF3
