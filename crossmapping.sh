d8ceb43bacd7ec234cbafa5a2cd75b541ea94e80
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-04-18 09:52:01 +0200

    Update crossmapping.sh

--- crossmapping.sh
+++ crossmapping.sh
@@ -1,3 +1,6 @@
 # https://github.com/Gabaldonlab/crossmapper
+# Installation
+# conda install -c gabaldonlab -c bioconda crossmapper
 
+# Running
 crossmapper RNA -t 4 -rlay PE -rlen 150 -o ~/Desktop -rc TRUE -gn GenomeName1 GenomeName2 GenomeName3 -g fasta Fasta1 Fasta2 Fasta3  -a gtf GTF1 GTF2 GTF3
