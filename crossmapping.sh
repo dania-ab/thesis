1d7d0348a9cf9f9579b8f32225a797e2f7cf88c7
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-04-22 19:19:48 +0200

    Delete crossmapping.sh

--- crossmapping.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/Gabaldonlab/crossmapper
-# Installation
-# conda install -c gabaldonlab -c bioconda crossmapper
-
-# Running
-crossmapper RNA -t 4 -rlay PE -rlen 150 -o ~/Desktop -rc TRUE -gn CA H SC -g fasta CA.fna human.fna SC.fna  -a gff albicans.gff human.gff sc.gff
