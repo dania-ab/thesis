eeb1797bfd6a5039bae187f670e8d1addda1bdd7
dania-ab <161978042+dania-ab@users.noreply.github.com>
2024-06-12 10:39:36 +0200

    Update fqc.sh

--- fqc.sh
+++ fqc.sh
@@ -1,7 +1,7 @@
 FILES=$(pwd)
 WKDIR=$(echo $FILES | sed 's:/required_files::g')
 for SNAME in $(ls $WKDIR | egrep '(\.f.*q$)|(\.q\.gz$)')
-    do
-      i=$WKDIR/$SNAME
-      fastqc -o $WKDIR/QC_raw $i
-    done
+do
+    i="$WKDIR/$SNAME"
+    fastqc -o "$WKDIR/QC_raw" "$i"
+done
